23a955ecc1cef3413096bb7d1cb7ea45dbf2acbf
Harsh-sharma07 <hs6102422@gmail.com>
2024-05-14 15:11:18 +0530

    ek orr

---
+++ day2.py
@@ -0,0 +1,52 @@
+#
+
+#print(var1=10)
+#print(var2=21.23)
+#print(var3="upflairs")
+#print(var4='jecrc')
+#print(var5="""jecrc foundation""")
+#print()
+#print(type(var1=10))
+#print(type(var2=21.23))
+#print(type(var3="upflairs"))
+#print(type(var4='jecrc'))
+#print(type(var5="""jecrc foundation"""))
+
+# college = "jecrc"
+# print(college.upper())
+# print(len(college))
+# print(college.count('c'))
+# print(college.endswith('c'))
+# print(college.startswith('j'))
+
+#college = "jecrc foundation"
+# print(college.strip())
+# print(college.lstrip())
+# print(college.rstrip())
+# j e c r c f o u n d a  t  i  o  n
+# 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14
+# print(college[12])
+# print(college[0:5])
+# print(college[0:4])
+
+# college = "jecrc"
+# j e c r c
+# o  1  2   3   4
+#-5 -4 -3  -2  -1
+# print(college[0:5:1])
+# print(college[0:5:2])
+# print(college[-3])
+# print(college[2: ])
+# print(college[:5])
+##for rc with both positive and negative indexing
+# print(college[3:5])
+# print(college[-2:])
+# print(college[-4:-2])
+##print crcej
+# print(college[::1])
+# print(college[::-1])
+# print(college[4::-1])
+clg = "jecrc jaipur rajasthan"
+print(clg[17:22])
+print(clg[-1:-5:-1])
+print(clg[-1])
