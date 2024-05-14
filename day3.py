1d8930e6bd9e45fc2b1b69f8be6e5407614c2c55
Harsh-sharma07 <136671691+Harsh-sharma07@users.noreply.github.com>
2024-05-14 14:09:36 +0530

    day 3 code uploaded

---
+++ day3.py
@@ -0,0 +1,78 @@
+# lst=[25,41,63,31.5,'hello',True]
+#print(lst)
+#print(lst[1:6])
+# lst[1]=500
+# print(lst)
+#print(len(lst))
+# lst.append(100)
+# print(lst)
+# lst.append('harsh')
+# print(lst)
+# lst.pop()
+# print(lst)
+# lst.insert(1,'jecrc')
+# print(lst)
+
+# lst=[25,41,63,31.5,'hello',True]
+# #print(lst[4][-1])
+# mess=lst[4]
+# print(mess[-1])
+
+#lst=[25,41,31.4,[100,200,300]]
+# lst[3][1] = 1000
+# print(lst)
+#print(lst[3][1])
+# lst.insert[[3][1],1000]
+# print(lst)
+
+# tpl=(10,20,30,40,50.3,'hello',True)
+# print(tpl)
+# print(type(tpl))
+#print(tpl[1])
+# tpl[1]=200
+# print(tpl)
+# college_name = ('jecrc',)
+# print(type(college_name))
+# clg_names = "jecrc",'vanasthali'
+# print(type(clg_names))
+# print(clg_names)
+# clg_name = "jecrc",
+# print(type(clg_name))
+
+# lst=[12,23,34]
+# lst.remove(23)
+# print(lst)
+
+# name="jecrc "
+# print(name*3)
+# roll=100
+# surname="foundation"
+# print(name+surname)
+# print(name+roll)
+##replication method
+# tpl2=(10,20,30)
+# print(tpl2*2)
+
+# lst=[30,40,50]
+# print(lst*3)
+
+# lst1=[1,2,3,4]
+# lst2=[9,8,7,6]
+# lst1.append(lst2)
+# print(lst1)
+
+# lst1=[1,2,3,4]
+# lst2=[9,8,7,6]
+# lst1.extend(lst2)
+# print(lst1)
+
+#key,values
+# dt={"name":"rohit","branch":'cse',"roll":1}
+# print(dt)
+# print(type(dt))
+# print(dt["name"])/
+# print(dt.keys())
+# print(dt.values())
+class_data={"name":["a","b","c","d"],"marks":[2,3,4,6]}
+#print(class_data)
+print(class_data["name"])
